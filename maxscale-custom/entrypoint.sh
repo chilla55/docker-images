e9c13703edebb7c97f820c1dc9939f94e8cc80dd
Alexander Grötzsch <a.g0201@gmx.de>
2025-12-10 19:44:25 +0100

    fix: Update MaxScale configuration handling and entrypoint script for improved secret substitution

--- maxscale-custom/entrypoint.sh
+++ maxscale-custom/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-# Substitute secrets into maxscale.cnf
+# Copy config from temp location to /etc/maxscale.cnf
+cp /tmp/maxscale.cnf /etc/maxscale.cnf
+
+# Substitute secrets into it
 if [ -f /run/secrets/mysql_root_password ]; then
     MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
     sed -i "s|\${MYSQL_ROOT_PASSWORD}|${MYSQL_ROOT_PASSWORD}|g" /etc/maxscale.cnf
 fi
 
-# Start MaxScale with passed arguments
-exec /usr/bin/maxscale "$@"
+# Start MaxScale with the edited config
+exec su -s /bin/bash maxscale -c "/usr/bin/maxscale $*"
