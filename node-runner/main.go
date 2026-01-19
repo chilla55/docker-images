a26a7947a537b69a9fc1eca1370725e51f84573a
Alexander Grötzsch <a.g0201@gmx.de>
2026-01-19 22:57:04 +0100

    Remove WebSocket option from registry route configuration

--- node-runner/main.go
+++ node-runner/main.go
@@ -228,7 +228,6 @@ func startRegistry(cfg config) (*registryclient.RegistryClientV2, string) {
 
 	_ = client.SetOptions("compression", "true")
 	_ = client.SetOptions("http2", "true")
-	_ = client.SetRouteOption(routeID, "websocket", "true")
 
 	if err := client.ApplyConfig(); err != nil {
 		log("failed to apply registry config: %v", err)
